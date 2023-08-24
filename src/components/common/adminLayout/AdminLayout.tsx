ebe0eba7a305534f61e13d100c67b4a38da8381a
jecinta2003 <onyemaobijecintaugochi@gmail.com>
2023-08-24 18:06:22 +0100

    {board{

--- src/components/common/adminLayout/AdminLayout.tsx
+++ src/components/common/adminLayout/AdminLayout.tsx
@@ -1,14 +1,17 @@
-// import React from 'react'
-import { Outlet } from "react-router"
+import { Outlet } from "react-router-dom"
 import Sider from "./Sider"
 
 const AdminLayout = () => {
   return (
-    <div className="flex">
-      <div>
-      <Sider/>
-      </div>
-      <Outlet />
+    <div>
+        <div className="flex">
+        <Sider/>
+        <div className='flex justify-end w-full'>
+        <div className="flex w-[calc(100vw-201px)] min-h-[100vh]">
+        <Outlet />
+        </div>
+        </div>
+        </div>
     </div>
   )
 }
